d9956de83d7ea556b69f49a43008bcc5d13191f2
Issakha5 <issakhad5@gmail.com>
2019-10-09 17:11:37 +0000

    registration Signup

--- blog1/views.py
+++ blog1/views.py
@@ -3,6 +3,14 @@ from django.utils import timezone
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
 
 
 def post_list(request):
